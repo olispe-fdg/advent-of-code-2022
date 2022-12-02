e63bf3793f960aa44586011b05316841becc5cce
olispe-fdg <115625185+olispe-fdg@users.noreply.github.com>
2022-12-02 10:20:39 +0000

    Read input file

--- 01/source/app.d
+++ 01/source/app.d
@@ -1,6 +1,32 @@
 import std.stdio;
+import core.stdc.errno;
+import std.exception;
 
-void main()
+void main(string[] args)
 {
-	writeln("Hello World");
+	if (args.length < 2) {
+		writeln("No file specified");
+		return;
+	}
+
+	try {
+		auto file = File(args[1], "r");
+		
+		foreach (line; file.byLine) {
+			writeln(line);
+		}
+	} catch (ErrnoException err) {
+		switch (err.errno) {
+			case EPERM:
+			case EACCES:
+				writeln("Could not open input file: Permission denied");
+				break;
+			case ENOENT:
+				writeln("Could not open input file: File does not exist");
+				break;
+			default:
+				writeln("Could not open input file: Unknown error occured");
+				break;
+		}
+	}
 }
