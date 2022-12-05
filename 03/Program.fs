c8586f98932ed9c65aba0c6c2cf00a14189baf9f
olispe-fdg <115625185+olispe-fdg@users.noreply.github.com>
2022-12-05 13:48:24 +0000

    Set up day 3

---
+++ 03/Program.fs
@@ -0,0 +1,12 @@
+﻿open System.IO
+
+[<EntryPointAttribute>]
+let main args =
+    let filePath = args.[0]
+
+    let readLines = File.ReadLines(filePath)
+
+    for line in readLines do
+        printfn "%s" line
+
+    0
---
+++ 03/README.md
@@ -0,0 +1,3 @@
+# Day 3 - F#
+
+## Remarks
