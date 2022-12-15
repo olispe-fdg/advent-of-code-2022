ce58d0ec7a860464cbc2d158a9e4138dbd5b0371
olispe-fdg <115625185+olispe-fdg@users.noreply.github.com>
2022-12-15 15:12:18 +0000

    Read stacks in and print out tops

--- 05/source/main.nim
+++ 05/source/main.nim
@@ -1,6 +1,49 @@
 import os
+import algorithm
+import nre
 
 let fileName = paramStr(1)
+let f = open(fileName)
 
-for line in lines fileName:
+type
+    Stack = seq[string]
+    Stacks = array[9, Stack]
+
+let stackExpr = re"(?:(?:    )|(?:\[([A-Z])\]))"
+let moveExpr = re"move ([0-9]+) from ([0-9]+) to ([0-9]+)"
+
+var stacks: Stacks
+
+# Read header
+var line: string
+while f.readLine(line):
+    var stackIdx = 0
+    var matchFound = false
+
+    for match in line.findIter(stackExpr):
+        if 0 in match.captureBounds:
+            stacks[stackIdx].add(match.captures[0])
+
+        stackIdx += 1
+        matchFound = true
+
+    if not matchFound: break
+
+# We read crates top to bottom, reverse to get top items at end of seq
+for stack in stacks.mitems:
+    stack.reverse()
+
+# Perform moves
+proc moveCrate(self: Stacks, fromStack: int, toStack: int) =
+    echo "NYI"
+
+while f.readLine(line):
     echo line
+
+# Print stack tops
+var output = ""
+for stack in stacks:
+    if stack.len > 0:
+        output &= stack[^1]
+
+echo output
